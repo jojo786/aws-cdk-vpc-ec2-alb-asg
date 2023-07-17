ccb6707beba8447524a88690ec02a89f85b32adc
Yusuf Mayet <jojo786@gmail.com>
2023-07-17 12:11:39 +0200

    Added tags. Added stacks per module: Applications and Admin

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 
-from aws_cdk import App
+from aws_cdk import App, Stack, Tags
 
 from cdk_vpc_ec2.cdk_vpc_stack import CdkVpcStack
-from cdk_vpc_ec2.cdk_ec2_stack import CdkEc2ApplicationsStack
-from cdk_vpc_ec2.cdk_rds_stack import CdkRdsApplicationsStack
+#Applications
+from cdk_vpc_ec2.cdk_ec2_applications_stack import CdkEc2ApplicationsStack
+from cdk_vpc_ec2.cdk_rds_applications_stack import CdkRdsApplicationsStack
+#Admin
+from cdk_vpc_ec2.cdk_ec2_admin_stack import CdkEc2AdminStack
+from cdk_vpc_ec2.cdk_rds_admin_stack import CdkRdsAdminStack
 
 app = App()
+Tags.of(app).add("Env", "Prod")
 
 vpc_stack = CdkVpcStack(app, "cdk-vpc-infinity")
-ec2_applictions_stack = CdkEc2ApplicationsStack(app, "cdk-ec2-infinity-applications",
+
+#Applications Module
+ec2_applications_stack = CdkEc2ApplicationsStack(app, "cdk-ec2-infinity-applications",
                         vpc=vpc_stack.vpc)
-rds_applictions_stack = CdkRdsApplicationsStack(app, "cdk-rds-infinity-applications",
+rds_applications_stack = CdkRdsApplicationsStack(app, "cdk-rds-infinity-applications",
                         vpc=vpc_stack.vpc,
-                        asg_security_groups=ec2_applictions_stack.asg_web.connections.security_groups)
+                        asg_security_groups=ec2_applications_stack.asg_web.connections.security_groups)
+Tags.of(ec2_applications_stack).add("Module", "Applications")
+Tags.of(rds_applications_stack).add("Module", "Applications")
+
+#Admin Module
+ec2_admin_stack = CdkEc2AdminStack(app, "cdk-ec2-infinity-admin",
+                        vpc=vpc_stack.vpc)
+rds_admin_stack = CdkRdsAdminStack(app, "cdk-rds-infinity-admin",
+                        vpc=vpc_stack.vpc,
+                        asg_security_groups=ec2_admin_stack.asg_web.connections.security_groups)
+Tags.of(ec2_admin_stack).add("Module", "Admin")
+Tags.of(rds_admin_stack).add("Module", "Admin")
 
 app.synth()
--- cdk_vpc_ec2/cdk_vpc_stack.py
+++ cdk_vpc_ec2/cdk_vpc_stack.py
@@ -6,35 +6,57 @@ class CdkVpcStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
-        # Infinity Applications Module: configuration will create 5 groups in 2 AZs = 10 subnets.
+        module1="Applications"
+        module2="Admin"
+        
         self.vpc = ec2.Vpc(self, "VPC-Infinity",
                            max_azs=2,
+                           nat_gateways=4,
                            ip_addresses=ec2.IpAddresses.cidr("10.10.0.0/16"),
                            subnet_configuration=[
+                               #Applications Module: configuration will create 5 groups in 2 AZs = 10 subnets
                                ec2.SubnetConfiguration(
                                subnet_type=ec2.SubnetType.PUBLIC,
-                               name="Public-Applications-Web-ALB",
+                               name=f"Public-{module1}-Web-ALB",
                                cidr_mask=24
                            ), ec2.SubnetConfiguration(
                                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
-                               name="Private-Applications-Web-ASG",
+                               name=f"Private-{module1}-Web-ASG",
                                cidr_mask=24
                            ), ec2.SubnetConfiguration(
                                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
-                               name="Private-Applications-API-ALB",
+                               name=f"Private-{module1}-API-ALB",
                                cidr_mask=24    
                            ), ec2.SubnetConfiguration(
                                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
-                               name="Private-Applications-API-ASG",
+                               name=f"Private-{module1}-API-ASG",
                                cidr_mask=24 
                            ), ec2.SubnetConfiguration(
                                subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
-                               name="DB-Applications",
+                               name=f"Private-DB-{module1}",
                                cidr_mask=24
-                           )
-                           ],
-                           nat_gateways=2,
+                               #Admin Module: configuration will create 5 groups in 2 AZs = 10 subnets.
+                           ), ec2.SubnetConfiguration(
+                               subnet_type=ec2.SubnetType.PUBLIC,
+                               name=f"Public-{module2}-Web-ALB",
+                               cidr_mask=24
+                           ), ec2.SubnetConfiguration(
+                               subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                               name=f"Private-{module2}-Web-ASG",
+                               cidr_mask=24
+                           ), ec2.SubnetConfiguration(
+                               subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                               name=f"Private-{module2}-API-ALB",
+                               cidr_mask=24    
+                           ), ec2.SubnetConfiguration(
+                               subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                               name=f"Private-{module2}-API-ASG",
+                               cidr_mask=24 
+                           ), ec2.SubnetConfiguration(
+                               subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                               name=f"Private-DB-{module2}",
+                               cidr_mask=24)
+                           ]
                            )
         CfnOutput(self, "Output",
                        value=self.vpc.vpc_id)
