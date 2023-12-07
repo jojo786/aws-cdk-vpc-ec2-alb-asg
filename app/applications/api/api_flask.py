8c2bbb87e5efd25887ef647ff52f5fc32d19fae2
Yusuf Mayet <jojo786@gmail.com>
2023-12-07 11:50:25 +0200

    flask app for web

--- app/applications/api/api_flask.py
+++ app/applications/api/api_flask.py
@@ -1,31 +1,13 @@
-#create new flask application with db connection to postgres
 import os
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
-from dataclasses import dataclass
 import psycopg2
-#from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:eqiQSWyCQjqIHPrTbmjS@database-1.cluster-csh5ndq6v79g.af-south-1.rds.amazonaws.com:5432'
-db = SQLAlchemy(app)
-#migrate = Migrate(app, db)
 
-@dataclass
-class Applications(db.Model):
-    student_name = str
-    student_email = str
-
-    #id = db.Column(db.Integer, primary_key=True)
-    #created = db.Column(db.TIMESTAMP(timezone=False))
-    student_name = db.Column(db.Text, primary_key=True)
-    student_email = db.Column(db.Text)
 
 @app.route('/applications')
 def index():
-    #applications = Applications.query.all()
     conn = psycopg2.connect(
                             user="postgres", 
                             password="eqiQSWyCQjqIHPrTbmjS", 
@@ -46,4 +28,4 @@ def index():
     conn.close() 
     for app in applications:
         print(app)
-    return jsonify(applications)  #render_template('index.html', applications=applications)
+    return jsonify(applications)
